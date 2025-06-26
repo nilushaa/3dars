16ff8ff7dc20dacccaec0c230acea48c966faf49
nilufar <nilufar@gmail.com>
2025-06-26 21:26:15 +0500

    s

--- src/App.jsx
+++ src/App.jsx
@@ -228,8 +228,9 @@ function App() {
  <div className="">
   <header className=" container pt-15 pb-10 leading-relaxed bg-purple-600  flex items-center justify-center">
         <ul class="centered-list">
-        <span
-              class=" rounded-[70] border-2 p-2 bg-fuchsia-400"
+       
+              <span
+              class="rounded-[50%] border-2 p-2 border-pink-300 text-pink-500 bg-fuchsia-400"
               >&Nilush</span>
             <li>O'ZBEK KITOBLAR</li>
             <li>XITOY KITOBLAR</li>
